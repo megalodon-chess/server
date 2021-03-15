1caf381fb7f51fb7446fb062b23e986514db6f90
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-15 12:00:07 -0700

    Data manager class.

--- movegen/server.py
+++ movegen/server.py
@@ -17,23 +17,48 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+import os
+import time
 import socket
 import threading
 
+PARENT = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = os.path.join(PARENT, "failed.txt")
 
-def client(conn, addr):
+
+class DataManager:
+    def __init__(self):
+        self.queue = []
+        threading.Thread(target=self.writer).start()
+
+    def write(self, msg):
+        self.queue.append(msg)
+
+    def writer(self):
+        while True:
+            time.sleep(0.01)
+            if len(self.queue) > 0:
+                msg = self.queue.pop(0)
+                with open(DATA_PATH, "a") as file:
+                    file.write(msg)
+                    file.write("\n")
+
+
+def client(conn, addr, dataman):
     length = int(conn.recv(64))
     moves = conn.recv(length).decode()
     conn.close()
+    dataman.write(moves)
 
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((input("IP: "), 5555))
     server.listen()
+    dataman = DataManager()
     while True:
         conn, addr = server.accept()
-        threading.Thread(target=client, args=(conn, addr)).start()
+        threading.Thread(target=client, args=(conn, addr, dataman)).start()
 
 
 main()
